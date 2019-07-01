257bf454534eade57a293bdbe7426b919917a474
Mariah M <37188233+mariahmcco@users.noreply.github.com>
2019-06-30 21:35:35 -0400

    Added words Mariah Mccormick after user presses space

---
+++ Hello Unity World/Assets/ChangeName.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChangeName : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        TextMeshProUGUI myText = GetComponent<TextMeshProUGUI>();
+        if (Input.GetKeyDown("space"))
+        {
+            myText.SetText("Hello World from Mariah McCormick"); 
+            Debug.Log ("Key Pressed");
+        
+        }
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            Application.Quit();
+        }
+        
+    }
+}
